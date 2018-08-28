9299a882b409d05bc2e2650e1a1f44149b86184d
Homeye <kornjoni@gmail.com>
2018-08-28 17:14:12 +0200

    already done this as a shorter course, project copied from there

---
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,28 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BinarySearchImpl {
+
+    @Autowired
+    private SortAlgorithm sortAlgorithm;
+
+//    public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+//        this.sortAlgorithm = sortAlgorithm;
+//    }
+
+    //Not necessary
+    public void setSortAlgorithm(SortAlgorithm sortAlgorithm) {
+        this.sortAlgorithm = sortAlgorithm;
+    }
+
+    public int binarySearch(int[] numbers, int numberToSearch) {
+//        BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
+//        int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+        int[] sortedNumbers = sortAlgorithm.sort(numbers);
+        System.out.println(sortAlgorithm);
+        return 3;
+    }
+}
